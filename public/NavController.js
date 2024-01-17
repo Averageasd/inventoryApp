2727b18ee6c9527770fb5c60517ed834e052d743
Nguyen Huynh <nhuynh9497@gmail.com>
2024-01-17 16:29:42 -0500

    finished styling

---
+++ public/NavController.js
@@ -0,0 +1,5 @@
+const navListBtn = document.querySelector('.nav-list-btn');
+navListBtn.addEventListener('click', function(e){
+    document.querySelector('.link-container').classList.toggle('link-container-invisible')
+    console.log('ayo');
+});
