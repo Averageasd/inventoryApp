cf6ea99054e9dce5d06fa44894e30c10088d9e45
Nguyen Huynh <nhuynh9497@gmail.com>
2024-01-17 17:04:09 -0500

    use host 0.0.0.0 so app works on railway

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.use('/inventory', inventoryRouter);
 app.use('/inventory/item', itemRouter);
 app.use('/inventory/category', categoryRouter);
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0',() => {
     console.log(`Example app listening on port ${port}`)
 });
