9e55e114d71b2aebbcc74fc85628cbf66520a70d
Nguyen Huynh <nhuynh9497@gmail.com>
2024-01-16 21:58:27 -0500

    style nav bar

--- model/itemModel.js
+++ model/itemModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
     item_name: {type: String, required: true, minLength: 3, maxLength: 30},
-    description: {type: String, required: true, minLength: 3, maxLength: 100},
+    description: {type: String, required: true, minLength: 3, maxLength: 500},
     price: {type: Number, required: true, min: 5, max: 100},
     quantity: {type: Number, required: true, min: 0, max: 10},
     category: {type: Schema.Types.ObjectId, ref: "Category"}
