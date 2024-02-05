78c828d832ce429fde96050220e17d31073e092c
Nguyen Huynh <nhuynh9497@gmail.com>
2024-02-05 13:15:38 -0500

    added some settings to publish app to vercel

--- api/index.js
+++ api/index.js
@@ -12,13 +12,13 @@ async function main() {
 
 main().catch((err) => console.log(err));
 
-const indexRouter = require('./route/index');
-const inventoryRouter = require('./route/inventory');
-const itemRouter = require('./route/item');
-const categoryRouter = require('./route/category');
+const indexRouter = require('../route/main');
+const inventoryRouter = require('../route/inventory');
+const itemRouter = require('../route/item');
+const categoryRouter = require('../route/category');
 
 // use view engine ejs
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
 // parse json data.
@@ -26,13 +26,13 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 // serve static files .css and .js
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use('/', indexRouter);
 app.use('/inventory', inventoryRouter);
 app.use('/inventory/item', itemRouter);
 app.use('/inventory/category', categoryRouter);
 
-app.listen(port, '0.0.0.0',() => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening on port ${port}`)
 });
